e48a1d1b9f9097e88c18a8cda77044dfdfb7dd83
awesomelewis2007 <60709927+awesomelewis2007@users.noreply.github.com>
2022-04-24 16:40:46 +0100

    Add files via upload

---
+++ src/system/__main__.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import pylog
+
+from system import Raise
+from system import interpreter
+from system import shell
+
+if __name__ == "__main__":
+    for i in sys.argv:
+        if i == "--shell":
+            shell.in_shell = True
+            shell.run()
+    if len(sys.argv) > 1:
+        if os.path.isfile(sys.argv[1]):
+            interpreter.interpreter(open(sys.argv[1]).read())
+        else:
+            Raise.error(name="File Not Found", message="File does not exist: "+sys.argv[1])
+    else:
+        print("Enter a file name to run")
